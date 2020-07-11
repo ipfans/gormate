b1510c5db1ef773b43b876c9052146a037d2c3bc
ipfans <ipfanscn@gmail.com>
2020-07-11 19:54:19 +0800

    fix: auto update value

--- model.go
+++ model.go
@@ -6,7 +6,7 @@ import (
 
 type Model struct {
 	ID        uint64         `json:"id" gorm:"primaryKey"`
-	CreatedAt TimeFormat     `json:"createdAt" gorm:"type:time"`
-	UpdatedAt TimeFormat     `json:"updatedAt" gorm:"type:time"`
+	CreatedAt TimeFormat     `json:"createdAt" gorm:"autoCreateTime;type:time"`
+	UpdatedAt TimeFormat     `json:"updatedAt" gorm:"autoUpdateTime;type:time"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
