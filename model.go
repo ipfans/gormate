7240793bee396e062f93bb21b6e4f689c6dd4085
ipfans <ipfanscn@gmail.com>
2020-07-11 19:51:50 +0800

    fix: define correct type

--- model.go
+++ model.go
@@ -4,10 +4,9 @@ import (
 	"gorm.io/gorm"
 )
 
-
 type Model struct {
 	ID        uint64         `json:"id" gorm:"primaryKey"`
-	CreatedAt TimeFormat     `json:"createdAt"`
-	UpdatedAt TimeFormat     `json:"updatedAt"`
+	CreatedAt TimeFormat     `json:"createdAt" gorm:"type:time"`
+	UpdatedAt TimeFormat     `json:"updatedAt" gorm:"type:time"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
